03cd539fb88bd6548785b6c3a59bb15287af6b0b
Hoss <hossshakiba@oulook.com>
2021-03-09 19:08:01 +0330

    Add some more tests to users app

--- recipe_blog/users/models.py
+++ recipe_blog/users/models.py
@@ -5,13 +5,19 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
-
+    """
+    Custom user model with unique email address and special membership attribute
+    """
     email = models.EmailField(_("email"), max_length=254, unique=True)
     is_special = models.DateTimeField(_("special member until"), default=timezone.now)
 
     REQUIRED_FIELDS = ['email',]
 
     def is_special_member(self):
+        """
+        returns True if a user is a special member.
+        spcial members have access to all the recipes.
+        """
         if self.is_superuser or self.is_staff:
             return True
         return self.is_special > timezone.now()
--- recipe_blog/users/tests/test_models.py
+++ recipe_blog/users/tests/test_models.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -31,3 +32,31 @@ class ModelTest(TestCase):
         self.assertTrue(user.is_active)
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+    def test_create_user_email_normalized(self):
+        """Test normalizing of user's email"""
+        User = get_user_model()
+        username = 'testuser'
+        email = 'test@TEST.com'
+        password = 'password'
+        user = User.objects.create_user(username=username, email=email, password=password)
+        self.assertEqual(user.email, email.lower())
+
+    def test_user_is_special(self):
+        """Test if is_specail_member fuction works fine for a user"""
+        User = get_user_model()
+        username = 'testuser'
+        email = 'test@test.com'
+        password = 'password'
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.is_special += timedelta(days=3)
+        self.assertTrue(user.is_special_member())
+
+    def test_super_user_is_special(self):
+        """Test if is_specail_member fuction works fine for a superuser"""
+        User = get_user_model()
+        username = 'superuser'
+        email = 'super@test.com'
+        password = 'superduperpassword'
+        user = User.objects.create_superuser(username=username, email=email, password=password)
+        self.assertTrue(user.is_special_member())
