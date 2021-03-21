ea3f4f201f3c50f84b5455cc03f8c9fcdde154b6
Hoss <hossshakiba@oulook.com>
2021-03-21 16:10:26 +0330

    Update Create & Update method

--- recipe_blog/categories/serializers.py
+++ recipe_blog/categories/serializers.py
@@ -12,8 +12,24 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('name', 'slug')
 
     def create(self, validated_data):
-        """Set name as the slug if it is blank"""
-        if not validated_data['slug']:
-            validated_data['slug'] = slugify(validated_data['name'])
+        """
+        name must be capitalized & name as the slug if it is blank
+        """
+        validated_data['name'] = validated_data['name'].capitalize()
+
+        if validated_data['slug']:
+            validated_data['slug'] = validated_data['slug'].lower()
+        else:
+            validated_data['slug'] = slugify(validated_data['name'].lower())
         return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        """
+        Lower slug field & capitalize name field after update
+        """
+        validated_data['name'] = validated_data['name'].capitalize()
+        validated_data['slug'] = validated_data['slug'].lower()
+        
+        return super().update(instance, validated_data)
+
     
