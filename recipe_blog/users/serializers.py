3eac69cb41afb79f73bbb269e177103882f4594f
Hoss <hossshakiba@oulook.com>
2021-03-26 20:32:13 +0430

    Create two different serializers for List & Retrive

--- recipe_blog/users/serializers.py
+++ recipe_blog/users/serializers.py
@@ -2,21 +2,30 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
-
+class UserListSerializer(serializers.ModelSerializer):
+    recipe_count = serializers.SerializerMethodField()
     class Meta:
         model = get_user_model()
-        fields = ('username', 'email', 'special member', 'special member until:')
-        extra_kwargs = {'special member until:': {
-            'read_only': True,
+        fields = ('username', 'email', 'recipe_count', 'is_special_member')
+        read_only_fields = ('recipe_count', )
+
+    def get_recipe_count(self, obj):
+        return obj.recipe_set.count()
+    
+    
+class UserProfileSerializer(UserListSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'recipe_count', 'is_special_member', 'special member until:')
+        extra_kwargs = {
+            'special member until:': {
             'format': 'date: %d-%m-%Y time: %H:%M:%S',
             'source': 'is_special'},
-            'special member': {'source': 'is_special_member'},
         }
+        read_only_fields = ('special member until:', )
 
     def __init__(self, *args, **kwargs):
-        """Delete special member until: field if user is not special"""
+        """Delete 'special member until:' field if user is not special"""
         super().__init__(*args, **kwargs)
-
         if not self.instance.is_special_member():
             del self.fields['special member until:']
