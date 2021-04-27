2d805e76a488643c6ac115944321543461d953a3
Hoss <hossshakiba@outlook.com>
2021-04-27 20:08:46 +0430

    Add url field to recipe serializer

--- recipe_blog/recipe/serializers.py
+++ recipe_blog/recipe/serializers.py
@@ -4,11 +4,13 @@ from .models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    
+    """Serializer for recipes"""
+    url = serializers.HyperlinkedIdentityField(view_name='recipes-detail')
+
     class Meta:
         model = Recipe
         fields = (
             'author', 'title', 'instruction', 'time_minutes', 
-            'categories', 'ingredients', 'image'
+            'categories', 'ingredients', 'image', 'url'
         )
         read_only_fields = ('author', )
